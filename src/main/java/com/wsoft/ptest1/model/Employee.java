d7432d5f4995560196250c368c501ef095964332
boubechtoula <dw_boubechtoula@esi.dz>
2020-01-12 17:10:19 +0100

    add jpa and h2 database

--- src/main/java/com/wsoft/ptest1/model/Employee.java
+++ src/main/java/com/wsoft/ptest1/model/Employee.java
@@ -6,13 +6,21 @@
 package com.wsoft.ptest1.model;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
  * @author b.walid
  */
+@Entity(name = "employee")
 public class Employee implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
     private String fonction;
     private int age;
@@ -53,6 +61,14 @@ public class Employee implements Serializable {
     @Override
     public String toString() {
         return "Employee{" + "name=" + name + ", fonction=" + fonction + ", age=" + age + '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
 }
